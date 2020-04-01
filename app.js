0ffd7ff79d06da91cc32a1ed9d71636839482256
BryantDavis1986 <bryant.davis@hotmail.com>
2020-03-31 19:15:47 -0700

    added a welcome and goodbye message

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
 'use strict'
-
+var Welcome;
+var GoodBye;
 var MyName;
 var MyAge;
 var MyCity;
 var MyPet;
 var SoftwareDeveloper;
 
-MyName= prompt( 'Is my name Bryant?').toLowerCase();
-if(MyName === 'yes') {
+Welcome = prompt('Welcome to my guessing game page where you get to test your knowledge of knwing me. What is your name?')
+alert('Welcome and goodluck ' + Welcome);
+
+MyName = prompt('Is my name Bryant?').toLowerCase();
+if (MyName === 'yes') {
     // console.log( MyName + ' my name is Bryant ')
-    alert( MyName + ' my name is Bryant ')
+    alert(MyName + ' my name is Bryant ')
 }
-else if(MyName === "no") {
+else if (MyName === "no") {
     // console.log(MyName + ' was the wrong answer')
     alert(MyName + ' was the wrong answer')
 }
@@ -21,12 +25,12 @@ else {
     alert('You didn\'t enter anything useful')
 };
 
-MyAge= prompt( 'Am I 34yrs old?').toLowerCase();
-if(MyAge === 'yes') {
+MyAge = prompt('Am I 34yrs old?').toLowerCase();
+if (MyAge === 'yes') {
     // console.log( MyName + ' I am 34yrs old ')
-    alert( MyAge + ' I am 34yrs old ')
+    alert(MyAge + ' I am 34yrs old ')
 }
-else if(MyAge === "no") {
+else if (MyAge === "no") {
     // console.log(MyName + ' was the wrong answer')
     alert(MyAge + ' was the wrong answer')
 }
@@ -36,12 +40,12 @@ else {
     alert('You didn\'t enter anything useful')
 };
 
-MyCity= prompt( 'Do I live in Tampa, Flordia?').toLowerCase();
-if(MyCity === 'yes') {
+MyCity = prompt('Do I live in Tampa, Flordia?').toLowerCase();
+if (MyCity === 'yes') {
     // console.log( MyName + ' was the wrong answer ')
-    alert( MyCity + ' was the wrong answer')
+    alert(MyCity + ' was the wrong answer')
 }
-else if(MyCity === "no") {
+else if (MyCity === "no") {
     // console.log(MyName + ' is the correct answer, I live in Seattle')
     alert(MyCity + ' is the correct answer, I live in Seattle')
 }
@@ -51,12 +55,12 @@ else {
     alert('You didn\'t enter anything useful')
 };
 
-MyPet= prompt( 'Do I own a dog named Rampage?').toLowerCase();
-if(MyPet === 'yes') {
+MyPet = prompt('Do I own a dog named Rampage?').toLowerCase();
+if (MyPet === 'yes') {
     // console.log( MyName + ' I do own a dog named Rampage ')
-    alert( MyPet + ' I do own a dog named Rampage ')
+    alert(MyPet + ' I do own a dog named Rampage ')
 }
-else if(MyPet === "no") {
+else if (MyPet === "no") {
     // console.log(MyName + ' was the wrong answer')
     alert(MyPet + ' was the wrong answer')
 }
@@ -66,12 +70,12 @@ else {
     alert('You didn\'t enter anything useful')
 };
 
-SoftwareDeveloper= prompt( 'Am I a Software Developer?').toLowerCase();
-if(SoftwareDeveloper === 'yes') {
+SoftwareDeveloper = prompt('Am I a Software Developer?').toLowerCase();
+if (SoftwareDeveloper === 'yes') {
     // console.log( MyName + '  was incorrect, I am in training to be one though ')
-    alert( SoftwareDeveloper + ' was incorrect, I am in training to be one though.')
+    alert(SoftwareDeveloper + ' was incorrect, I am in training to be one though.')
 }
-else if(SoftwareDeveloper === "no") {
+else if (SoftwareDeveloper === "no") {
     // console.log(MyName + ' was the right answer')
     alert(SoftwareDeveloper + ' was the right answer')
 }
@@ -80,3 +84,6 @@ else {
     // console.log('You didn\'t enter anything useful')
     alert('You didn\'t enter anything useful')
 };
+
+GoodBye = prompt('Thank you for testing your knowledge about me. Do you think you passed?')
+alert('Of course you did!!!!!!');
